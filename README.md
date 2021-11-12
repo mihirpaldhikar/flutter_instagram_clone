5b0d6e536be394fdd2eb03152d950d72ca3d8100
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2021-11-12 11:15:59 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Instagram Clone
+
+The project is a Instagram clone built with the [Flutter](https://flutter.dev).
---
+++ android/app/src/main/kotlin/com/imihirpaldhikar/flutter/instagram/flutter_instagram_clone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.imihirpaldhikar.flutter.instagram.flutter_instagram_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
